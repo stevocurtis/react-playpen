597526c96b7098cad64df3fa0bafb90a60510b0b
Steve Curtis <steven.curtis@bt.com>
2021-01-21 18:01:28 +0000

    Added form capture functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <Form/>
-      <TodoList/>
+      <header>
+        <h1>Steve's Todo List</h1>
+      </header>
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
+      <TodoList setTodos={setTodos} />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,20 +1,11 @@
 import React from 'react';
 
 function TodoList() {
+
+setTodos
+
   return (
-    <form>
-      <input type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
-        <i className="fas fa-plus-square"></i>
-      </button>
-      <div className="select">
-        <select name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
-    </form>
+    <p>Todolist Component</p>
   );
 }
 
